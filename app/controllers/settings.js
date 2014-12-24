ffafa7f9a9c52db834ca53bce4e9b3d88abe1b4d
Jordan Ranson <jordan@jordanranson.com>
2014-12-23 23:21:17 -0700

    Cleanup

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -82,7 +82,7 @@ module.exports = {
 
                                     user.save();
                                 }
-                                
+
                                 res.redirect('/admin');
                             });
                         });
